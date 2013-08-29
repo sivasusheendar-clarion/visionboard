cd84280dd3785ea2e472d8e600006345bd06bcd4
sivasusheendar-clarion <sivasusheendar.t@clariontechnologies.co.in>
2013-08-29 11:39:02 +0530

    added image modules

---
+++ application/modules/images/models/mdl_images.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/*
+ * FusionInvoice
+ * 
+ * A free and open source web based invoicing system
+ *
+ * @package		FusionInvoice
+ * @author		Jesse Terry
+ * @copyright	Copyright (c) 2012 - 2013, Jesse Terry
+ * @license		http://www.fusioninvoice.com/support/page/license-agreement
+ * @link		http://www.fusioninvoice.com
+ * 
+ */
+
+class Mdl_Images extends Response_Model {
+
+    public $table               = 'cb_images';
+    public $primary_key         = 'cb_images.id';
+
+
+   
+
+}
+
+?>
